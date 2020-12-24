6db9482a0140cac49687398637dfd25c2ae97d94
wangyingnan <487132988@qq.com>
2020-12-24 18:49:04 +0800

    初始化,ts

---
+++ src/components/son.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>这是son</div>
+</template>
+
+<script>
+import { Vue } from "vue-property-decorator";
+export default class Son extends Vue{
+    
+}
+</script>
+
+<style>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//vant
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
+Vue.use(Vant);
+
 
 Vue.config.productionTip = false
 
